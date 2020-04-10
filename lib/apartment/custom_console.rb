b9e46777a9beb7e58b908884ed407eef0c04a950
Jeremiah <jeremiahchurch@gmail.com>
2020-04-10 15:31:46 -0700

    Fix pry Depr warning in custom console

--- lib/apartment/custom_console.rb
+++ lib/apartment/custom_console.rb
@@ -14,13 +14,13 @@ module Apartment
       desc = "Includes the current Rails environment and project folder name.\n" \
             '[1] [project_name][Rails.env][Apartment::Tenant.current] pry(main)>'
 
-      Pry::Prompt.add 'ros', desc, %w[> *] do |target_self, nest_level, pry, sep|
+      prompt = Pry::Prompt.new 'ros', desc, %w[> *] do |target_self, nest_level, pry, sep|
         "[#{pry.input_ring.size}] [#{PryRails::Prompt.formatted_env}][#{Apartment::Tenant.current}] " \
         "#{pry.config.prompt_name}(#{Pry.view_clip(target_self)})" \
         "#{":#{nest_level}" unless nest_level.zero?}#{sep} "
       end
 
-      Pry.config.prompt = Pry::Prompt[:ros][:value]
+      Pry.config.prompt = prompt
     end
   end
 end
