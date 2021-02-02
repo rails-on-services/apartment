a3f231cc7fde858a5556ea3b09596cafb4435119
zephleggett <zephleggett@gmail.com>
2021-02-02 13:47:31 -0600

    Monkey patch ActiveRecord::Migrator to allow parallel ros-apartment migrations

---
+++ lib/apartment/active_record/migrator.rb
@@ -0,0 +1,16 @@
+# Monkey patch ActiveRecord::Migrator to allow parallel ros-apartment migrations
+# see -> https://github.com/rails/rails/pull/40251
+# TODO -> Remove whenever ros-apartment or rails implements a fix for threaded migrations
+
+module ActiveRecord
+  class Migrator < ActiveRecord # :nodoc:
+    class << self
+      def generate_migrator_advisory_lock_id
+        hash_input = ActiveRecord::Base.connection.current_database
+        hash_input += ActiveRecord::Base.connection.current_schema
+        db_name_hash = Zlib.crc32(hash_input)
+        MIGRATOR_SALT * db_name_hash
+      end
+    end
+  end
+end
