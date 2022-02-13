34f6cfcd65861198974a0d8375c4fcd8cefd2a6c
Samuel Sieg <samuel.sieg@crafted.ch>
2022-02-13 11:42:03 +0100

    Remove deprecated `database:` keyword from connection handling monkey patch

--- lib/apartment/active_record/connection_handling.rb
+++ lib/apartment/active_record/connection_handling.rb
@@ -5,10 +5,10 @@ module ActiveRecord
   # switches to the same tenant as before the connection switching. This problem is more evident when
   # using read replica in Rails 6
   module ConnectionHandling
-    def connected_to_with_tenant(database: nil, role: nil, prevent_writes: false, &blk)
+    def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
       current_tenant = Apartment::Tenant.current
 
-      connected_to_without_tenant(database: database, role: role, prevent_writes: prevent_writes) do
+      connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
         Apartment::Tenant.switch!(current_tenant)
         yield(blk)
       end
