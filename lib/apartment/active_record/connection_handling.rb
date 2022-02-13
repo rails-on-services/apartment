a5c86ff0033f3a7eb4428a1bc551260f53f7a4b1
Samuel Sieg <samuel.sieg@crafted.ch>
2022-02-13 11:44:38 +0100

    Only include the connection handling monkey patch for ActiveRecord >= 6
    The monkey patched `connected_to` was introduced in Rails 6, so this monkey patch isn't needed for Rails 5.

--- lib/apartment/active_record/connection_handling.rb
+++ lib/apartment/active_record/connection_handling.rb
@@ -4,17 +4,19 @@ module ActiveRecord
   # This is monkeypatching activerecord to ensure that whenever a new connection is established it
   # switches to the same tenant as before the connection switching. This problem is more evident when
   # using read replica in Rails 6
-  module ConnectionHandling
-    def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
-      current_tenant = Apartment::Tenant.current
+  if ActiveRecord::VERSION::MAJOR >= 6
+    module ConnectionHandling
+      def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
+        current_tenant = Apartment::Tenant.current
 
-      connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
-        Apartment::Tenant.switch!(current_tenant)
-        yield(blk)
+        connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
+          Apartment::Tenant.switch!(current_tenant)
+          yield(blk)
+        end
       end
-    end
 
-    alias connected_to_without_tenant connected_to
-    alias connected_to connected_to_with_tenant
+      alias connected_to_without_tenant connected_to
+      alias connected_to connected_to_with_tenant
+    end
   end
 end
