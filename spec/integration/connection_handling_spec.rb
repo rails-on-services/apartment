34fc2c545e428d2403741c8d7cd5c0c3fc5f79fe
Samuel Sieg <samuel.sieg@crafted.ch>
2022-02-16 09:49:38 +0100

    Simplify & make rubocop happy

--- spec/integration/connection_handling_spec.rb
+++ spec/integration/connection_handling_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'connection handling monkey patch' do
-  let(:db_names) { [db1, db2] }
+  let(:db_name) { db1 }
 
   before do
     Apartment.configure do |config|
@@ -12,48 +14,42 @@ describe 'connection handling monkey patch' do
 
     Apartment::Tenant.reload!(config)
 
-    db_names.each do |db_name|
-      Apartment::Tenant.create(db_name)
-      Company.create database: db_name
-      Apartment::Tenant.switch! db_name
-      User.create! name: db_name
-    end
+    Apartment::Tenant.create(db_name)
+    Company.create database: db_name
+    Apartment::Tenant.switch! db_name
+    User.create! name: db_name
   end
 
   after do
-    db_names.each { |db| Apartment::Tenant.drop(db) }
+    Apartment::Tenant.drop(db_name)
     Apartment::Tenant.reset
     Company.delete_all
   end
 
-  context 'ActiveRecord 5.x', if: ActiveRecord::VERSION::MAJOR == 5 do
+  context 'when ActiveRecord 5.x', if: ActiveRecord::VERSION::MAJOR == 5 do
     it 'is not monkey patched' do
-      expect(ActiveRecord::ConnectionHandling.instance_methods).to_not include(:connected_to_with_tenant)
+      expect(ActiveRecord::ConnectionHandling.instance_methods).not_to include(:connected_to_with_tenant)
     end
   end
 
-  context 'ActiveRecord >= 6.0', if: ActiveRecord::VERSION::MAJOR >= 6 do
+  context 'when ActiveRecord >= 6.0', if: ActiveRecord::VERSION::MAJOR >= 6 do
+    let(:role) do
+      # Choose the role depending on the ActiveRecord version.
+      case ActiveRecord::VERSION::MAJOR
+      when 6 then ActiveRecord::Base.writing_role # deprecated in Rails 7
+      else ActiveRecord.writing_role
+      end
+    end
+
     it 'is monkey patched' do
       expect(ActiveRecord::ConnectionHandling.instance_methods).to include(:connected_to_with_tenant)
     end
 
     it 'switches to the previous set tenant' do
-      # Choose the role depending on the ActiveRecord version.
-      role = case ActiveRecord::VERSION::MAJOR
-             when 6 then ActiveRecord::Base.writing_role # deprecated in Rails 7
-             else ActiveRecord.writing_role
-             end
-
-      Apartment::Tenant.switch! db_names.first
+      Apartment::Tenant.switch! db_name
       ActiveRecord::Base.connected_to(role: role) do
-        expect(Apartment::Tenant.current).to eq db_names.first
-        expect(User.find_by(name: db_names.first).name).to eq(db_names.first)
-      end
-
-      Apartment::Tenant.switch! db_names.last
-      ActiveRecord::Base.connected_to(role: role) do
-        expect(Apartment::Tenant.current).to eq db_names.last
-        expect(User.find_by(name: db_names.last).name).to eq(db_names.last)
+        expect(Apartment::Tenant.current).to eq db_name
+        expect(User.find_by(name: db_name).name).to eq(db_name)
       end
     end
   end
