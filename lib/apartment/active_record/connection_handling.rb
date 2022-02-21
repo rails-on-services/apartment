12d4ff8ee949024ecbed9b8f0734d176503df027
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-02-21 23:08:01 +0800

    fix methods signatures for rails less or equal to 6.2

--- lib/apartment/active_record/connection_handling.rb
+++ lib/apartment/active_record/connection_handling.rb
@@ -5,12 +5,23 @@ module ActiveRecord # :nodoc:
   # switches to the same tenant as before the connection switching. This problem is more evident when
   # using read replica in Rails 6
   module ConnectionHandling
-    def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
-      current_tenant = Apartment::Tenant.current
+    if ActiveRecord.version.release <= Gem::Version.new('6.2')
+      def connected_to_with_tenant(database: nil, role: nil, prevent_writes: false, &blk)
+        current_tenant = Apartment::Tenant.current
 
-      connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
-        Apartment::Tenant.switch!(current_tenant)
-        yield(blk)
+        connected_to_without_tenant(database: database, role: role, prevent_writes: prevent_writes) do
+          Apartment::Tenant.switch!(current_tenant)
+          yield(blk)
+        end
+      end
+    else
+      def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
+        current_tenant = Apartment::Tenant.current
+
+        connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
+          Apartment::Tenant.switch!(current_tenant)
+          yield(blk)
+        end
       end
     end
 
