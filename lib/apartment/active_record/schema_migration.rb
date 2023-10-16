2e953b11e8022627ba174112175ebc0afefa280b
Amnesthesia <flowsnake@icloud.com>
2023-10-16 11:59:12 +1000

    fix: conditionally remove superclass from schema migration

--- lib/apartment/active_record/schema_migration.rb
+++ lib/apartment/active_record/schema_migration.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
+require 'active_record/schema_migration'
 
 module ActiveRecord
-  class SchemaMigration
-    class << self
-      def table_exists?
-        connection.table_exists?(table_name)
+  if ActiveRecord::SchemaMigration < ActiveRecord::Base
+    class SchemaMigration < ActiveRecord::Base
+      class << self
+        def table_exists?
+          connection.table_exists?(table_name)
+        end
+      end
+    end
+  else
+    class SchemaMigration
+      class << self
+        def table_exists?
+          connection.table_exists?(table_name)
+        end
       end
     end
   end
