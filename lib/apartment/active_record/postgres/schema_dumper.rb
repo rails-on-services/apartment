81f3954100182934e24b31864f754e773557a13a
Nick Giancola <patbenatar@gmail.com>
2023-10-27 14:32:55 -0700

    Prevent Rails 7.1 create_schema from being added to db/schema.rb as schemas are managed by Apartment not ActiveRecord like they would be in a vanilla Rails setup.

---
+++ lib/apartment/active_record/postgres/schema_dumper.rb
@@ -0,0 +1,9 @@
+# This patch prevents `create_schema` from being added to db/schema.rb as schemas are managed by Apartment
+# not ActiveRecord like they would be in a vanilla Rails setup.
+
+require "active_record/connection_adapters/postgresql/schema_dumper"
+
+class ActiveRecord::ConnectionAdapters::PostgreSQL::SchemaDumper
+  def schemas(stream)
+  end
+end
