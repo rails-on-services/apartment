bc342e1813539d8ea32eb9284a72209f4ec9c8f4
Nick Giancola <patbenatar@gmail.com>
2024-09-19 12:38:49 -0700

    Need to also require the abstract superclass

--- lib/apartment/active_record/postgres/schema_dumper.rb
+++ lib/apartment/active_record/postgres/schema_dumper.rb
@@ -1,6 +1,7 @@
 # This patch prevents `create_schema` from being added to db/schema.rb as schemas are managed by Apartment
 # not ActiveRecord like they would be in a vanilla Rails setup.
 
+require "active_record/connection_adapters/abstract/schema_dumper"
 require "active_record/connection_adapters/postgresql/schema_dumper"
 
 class ActiveRecord::ConnectionAdapters::PostgreSQL::SchemaDumper
