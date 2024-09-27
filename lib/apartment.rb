0c0faf1c796d89108c76856903df5c30fcd2fb50
Nick Giancola <patbenatar@gmail.com>
2024-09-27 07:56:05 -0700

    Prevent Rails 7.1 create_schema from being added to db/schema.rb (#276)
    * Prevent Rails 7.1 create_schema from being added to db/schema.rb as schemas are managed by Apartment not ActiveRecord like they would be in a vanilla Rails setup.
    
    * Need to also require the abstract superclass
    
    * Only suppress create_schema when use_schemas = true

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -12,6 +12,10 @@ require_relative 'apartment/active_record/connection_handling'
 require_relative 'apartment/active_record/schema_migration'
 require_relative 'apartment/active_record/internal_metadata'
 
+if ActiveRecord.version.release >= Gem::Version.new('7.1')
+  require_relative 'apartment/active_record/postgres/schema_dumper'
+end
+
 # Apartment main definitions
 module Apartment
   class << self
