7dbbc919660939ed466ab7a8fca20c39fc4fd5f7
lit-poks <pokhrel.lalit99@gmail.com>
2024-10-09 12:57:05 +0600

    fix: Deprecation cannot find version error

--- lib/apartment/deprecation.rb
+++ lib/apartment/deprecation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/deprecation'
-
+require_relative 'version'
 module Apartment
   DEPRECATOR = ActiveSupport::Deprecation.new(Apartment::VERSION, 'Apartment')
 end
