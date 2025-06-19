d47f42de8c18b524fa40e205f2eb06e8826450c2
Jorge Vargas Silva <jvargas@pagerduty.com>
2025-06-19 18:18:11 -0400

    Handle log subscriber

--- lib/apartment/log_subscriber.rb
+++ lib/apartment/log_subscriber.rb
@@ -1,3 +1,4 @@
+
 # frozen_string_literal: true
 
 require 'active_record/log_subscriber'
@@ -28,18 +29,22 @@ module Apartment
     end
 
     def current_search_path
-      if Apartment.connection.respond_to?(:schema_search_path)
-        Apartment.connection.schema_search_path
-      else
-        Apartment::Tenant.current # all others
+      Apartment.with_connection do |conn|
+        conn.respond_to?(:schema_search_path) ? conn.schema_search_path : Apartment::Tenant.current
       end
     end
 
     def database_name
-      db_name = Apartment.connection.raw_connection.try(:db) # PostgreSQL, PostGIS
-      db_name ||= Apartment.connection.raw_connection.try(:query_options)&.dig(:database) # Mysql
-      db_name ||= Apartment.connection.current_database # Failover
-      db_name
+      Apartment.with_connection do |conn|
+        case conn.adapter_name
+        when "PostgreSQL", "PostGIS", "Mysql2"
+          conn.current_database
+        when "SQLite"
+          conn.connection_db_config.database # returns path or memory
+        else
+          raise NotImplementedError, "Adapter #{conn.adapter_name} unsupported for logging"
+        end
+      end
     end
   end
 end
