aad9b3bdf6bae1f2d605198dc2cfa354a059e4b5
Mauricio Novelo <5916364+mnovelo@users.noreply.github.com>
2025-11-23 19:49:16 -0500

    Fix legacy_connection_handling setting for Rails 8.0+
    The legacy_connection_handling config option was removed in Rails 8.0.
    Update conditional to only set it for Rails 7.0-7.x where it exists.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -48,8 +48,10 @@ module Dummy
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Use new connection handling for Rails 7.0+
+    # Use new connection handling for Rails 7.0-7.x (setting removed in Rails 8.0)
     # This silences the deprecation warning about legacy_connection_handling
-    config.active_record.legacy_connection_handling = false if ActiveRecord.version >= Gem::Version.new('7.0.0')
+    if ActiveRecord.version >= Gem::Version.new('7.0.0') && ActiveRecord.version < Gem::Version.new('8.0.0')
+      config.active_record.legacy_connection_handling = false
+    end
   end
 end
