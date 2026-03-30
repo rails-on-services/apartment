19a56ec85de045d1cc4d85172f05a2dede58e144
Mauricio Novelo <5916364+mnovelo@users.noreply.github.com>
2026-03-30 15:09:56 -0400

    v4 Host elevator: constructor keyword args, remove class-level setters

--- lib/apartment/elevators/host.rb
+++ lib/apartment/elevators/host.rb
@@ -4,31 +4,18 @@ require 'apartment/elevators/generic'
 
 module Apartment
   module Elevators
-    #   Provides a rack based tenant switching solution based on the host
-    #   Assumes that tenant name should match host
-    #   Strips/ignores first subdomains in ignored_first_subdomains
-    #   eg. example.com       => example.com
-    #       www.example.bc.ca => www.example.bc.ca
-    #   if ignored_first_subdomains = ['www']
-    #       www.example.bc.ca => example.bc.ca
-    #       www.a.b.c.d.com   => a.b.c.d.com
-    #
+    # Tenant from full hostname. Optionally strips ignored first subdomains (e.g., www).
     class Host < Generic
-      def self.ignored_first_subdomains
-        @ignored_first_subdomains ||= []
+      def initialize(app, ignored_first_subdomains: [], **_options)
+        super(app)
+        @ignored_first_subdomains = Array(ignored_first_subdomains).map(&:to_s).freeze
       end
-
-      # rubocop:disable Style/TrivialAccessors
-      def self.ignored_first_subdomains=(arg)
-        @ignored_first_subdomains = arg
-      end
-      # rubocop:enable Style/TrivialAccessors
 
       def parse_tenant_name(request)
         return nil if request.host.blank?
 
         parts = request.host.split('.')
-        self.class.ignored_first_subdomains.include?(parts[0]) ? parts.drop(1).join('.') : request.host
+        @ignored_first_subdomains.include?(parts[0]) ? parts.drop(1).join('.') : request.host
       end
     end
   end
